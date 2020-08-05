0a23a1340d14e9e3bb0325609b8dfddaec7e2ac8
AnooshaIA <anooshaia16@gmail.com>
2020-08-05 14:05:39 +0530

    add =ing 1st project

---
+++ Letter-count.py
@@ -0,0 +1,11 @@
+#This program counts no of letter present in the message
+print(" Welcome!")
+name = input("What is your name:").title().strip()
+print("Hello",name,"!")
+print("Will count the no.of letters present in your message.")
+print()
+message=input("Please enter a message:")
+let=input("which letter do you wanna count?:").lower()
+message=message.lower()
+res=message.count(let)
+print(res) 
